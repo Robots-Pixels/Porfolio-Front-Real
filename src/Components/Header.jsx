e1cef4a8673faa508eb54cc6e01e055008fd130a
Robots-Pixels <Robots-Pixels>
2025-03-26 13:22:18 +0100

    Making some UI fixes

---
+++ src/Components/Header.jsx
@@ -0,0 +1,158 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { FaBars, FaChevronLeft, FaChevronRight, FaEnvelope, FaGithub, FaLinkedin, FaWhatsapp } from 'react-icons/fa';
+import { HashLink } from 'react-router-hash-link';
+
+export default function Header({setTransform}) {
+
+    const [isSmall, setIsSmall] = useState(false);
+    const [clicked, setClicked] = useState(false);
+
+    useEffect(() => {
+        const handleWindowResize = () => {
+            if (window.innerWidth > 600){
+                setIsSmall(false);
+            }
+            else{
+                setIsSmall(true);
+            }
+        }
+        
+        handleWindowResize();
+
+        window.addEventListener("resize", handleWindowResize);
+        return () => {window.removeEventListener("resize", handleWindowResize)};
+    }, []);
+
+    const handleClick = () => {
+        setClicked(!clicked);
+    }
+
+    useEffect(() => {
+        if(clicked){
+            setTransform("13");
+        }
+        else{
+            setTransform("0");
+        }
+    }, [clicked]);
+
+    useEffect(() => {
+        if(!isSmall){
+            setClicked(false);
+        }
+    }, [isSmall])
+
+  return (
+
+    <div className='w-full'>
+    
+        <header className='relative py-5'>
+
+            <div className='max-w-3xl md:max-w-6xl mx-auto px-6 md:px-8'>
+                <div className='flex items-center justify-between'>
+
+                    <div className='flex items-center gap-6'>
+
+                        <Link className='text-xl logo' to={"/"}>
+
+                            <div className='flex items-center gap-1'>
+                                <h4 className='flex items-center'>
+                                    <FaChevronLeft className='text-sm text-[#052935]'/>
+                                    <span className='text-[#028041]'>O</span>
+                                    <span className='text-[#052935]'>T</span>
+                                    <FaChevronRight className='text-sm text-[#052935]'/>
+                                </h4>
+                                
+                                <h4 className='text-[#052935]'>Otmar Tchenga</h4>
+                            </div>
+
+                        </Link>
+
+                        <div className={` ${isSmall ? "hidden" : "flex"} items-center gap-6 text-[#052935]`}>
+                            
+                            <HashLink smooth to={"#about"}>
+                                About me
+                            </HashLink>
+
+                            <HashLink smooth to={"#portfolio"}>
+                                My Work
+                            </HashLink>
+                        
+                            <HashLink smooth to={"#services"}>
+                                My Services
+                            </HashLink>
+                        </div>
+
+                    </div>
+
+                    <div className={`${isSmall ? "hidden" : "flex"} items-center gap-4`}>
+                        <HashLink smooth to={"#contact"} className='border-[#028041] border-1 text-[#028041] px-4 py-2 text-[1rem] rounded-xl shadow-3xs shadow-[#028041] hover:bg-[#028041] hover:text-[#fff] transition-colors'>
+                            Contact Me
+                        </HashLink>
+                    </div>
+
+                    <div className={`${isSmall ? "block" : "hidden"} border-1 p-2 rounded-sm cursor-pointer`}
+                    onClick={handleClick}>
+                        <FaBars className='text-sm'/>
+                    </div>
+
+                    
+                </div>
+            </div>
+
+            <div className={`${isSmall ? "absolute" : "hidden"} w-full h-[12rem] left-0 top-[100%] pt-9 pb-8`}>
+
+                <div className='max-w-3xl md:max-w-6xl mx-auto px-6 md:px-8 h-full'>
+
+                    <div className={`flex flex-col gap-3 text-[#052935] h-full justify-center`}>
+                        
+                        <HashLink smooth to={"#about"}>
+                            About me
+                        </HashLink>
+
+                        <HashLink smooth to={"#portfolio"}>
+                            My Work
+                        </HashLink>
+
+                        <HashLink smooth to={"#services"}>
+                            My Services
+                        </HashLink>
+
+                        <div className='flex items-center gap-2'>
+
+                            <Link className='hover:scale-120 transition-all' to={"https://github.com/Robots-Pixels"}>
+                                <FaLinkedin className='text-2xl'/>
+                            </Link>
+
+                            <Link className='hover:scale-120 transition-all' to={"/https://www.linkedin.com/in/otmar-tchenga-953988319?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"}>
+                                <FaGithub className='text-2xl'/>
+                            </Link>
+
+                            <Link className='hover:scale-120 transition-all' to={"mailto:otmartch23@gmail.com"}>
+                                <FaEnvelope className='text-2xl'/>
+                            </Link>
+
+                            <Link className='hover:scale-120 transition-all' to={"https://wa.me/2290140820493"}>
+                                <FaWhatsapp className='text-2xl'/>
+                            </Link>
+
+                        </div>
+
+                        <div className='mt-2 -ml-1'>
+                            <HashLink smooth to={"#contact"} className='border-[#028041] border-1 text-[#028041] px-5 py-2 text-[1rem] rounded-xl shadow-3xs shadow-[#028041] hover:bg-[#028041] hover:text-[#fff] transition-colors'>
+                                Contact Me
+                            </HashLink>
+                        </div>
+                            
+                    </div>
+
+                </div>
+            </div>
+
+        </header>
+        
+    </div>
+
+  )
+}
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,396 @@
+import React, { useEffect, useState } from 'react'
+import { FaEnvelope, FaGithub, FaLinkedin, FaDotCircle, FaArrowRight, FaEye, FaWhatsapp } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import Footer from '../Components/Footer';
+import { HashLink } from 'react-router-hash-link';
+import { useNavigate } from 'react-router-dom'
+
+export default function Home({transform, success,  setSucces}) {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: ""
+    })
+    const navigate = useNavigate();
+
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value
+        });
+    }
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        const base = import.meta.env.VITE_BACKEND_API_BASE_URL;
+        const res = await fetch(`${base}/api/message/newMessage`,
+            {
+                method: "POST", 
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData)
+            }
+        )
+
+        const data = await res.json();
+        if (data.success === false){
+            setError(data.message);
+            setLoading(false);
+            return;
+        }
+
+        if (data.success === true){
+            setSucces(data.message);
+            setLoading(false);
+        }
+
+        setTimeout(() => {
+            navigate("/");
+        }, 2000);
+    }
+
+    useEffect(() => {
+        if (success){
+            setFormData({
+                name: "",
+                email: "",
+                message: ""
+            })
+            setTimeout(()=> {
+                setSucces(false);
+            }, 2000)
+        }
+    }, [success]);
+
+  return (
+
+    <div className={`relative bg-[#faf8ff] w-full`}
+    style={{transition: "all 0.5s ease", transform: `translateY(${transform}rem)`}}>
+
+        <section id='hero' className='max-w-3xl md:max-w-6xl mx-auto px-6 md:px-8] sm:pb-20'>
+
+            <div className='flex flex-col sm:flex-row gap-4 sm:gap-0  sm:h-[70vh] items-center py-10 sm:py-0'>
+
+                <div className='flex flex-1 flex-col gap-3'>
+
+                    <h3 className='text-xl sm:text-3xl'>
+                        Hi, I'm
+                    </h3>
+
+                    <h2 className='text-4xl sm:text-6xl'>
+                        Web Developer<span className='text-[#028041]'>.</span>
+                    </h2>
+
+                    <h4 className='mb-4'>My name is Otmar — I design and develop scalable digital products, blending precision with intuitive user experiences.</h4>
+
+                    <div className='hidden sm:flex items-center gap-2 sm:gap-4'>
+
+                        <div className='flex gap-2'>
+
+                        <Link className='hover:scale-120 transition-all' to={"/https://www.linkedin.com/in/otmar-tchenga-953988319?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"} >
+                            <FaLinkedin className='text-2xl'/>
+                        </Link>
+
+                        <Link className='hover:scale-120 transition-all' to={"https://github.com/Robots-Pixels"}>
+                            <FaGithub className='text-2xl'/>
+                        </Link>
+
+                        <Link className='hover:scale-120 transition-all' to={"mailto:otmartch23@gmail.com"}>
+                            <FaEnvelope className='text-2xl'/>
+                        </Link>
+
+                        <Link className='hover:scale-120 transition-all' to={"https://wa.me/2290140820493"}>
+                            <FaWhatsapp className='text-2xl'/>
+                        </Link>
+
+                
+                        </div>
+
+
+                        <div className=''>
+                            <HashLink smooth to={"#contact"} className='border-[#028041] border-1 text-[#028041] px-2 py-2 text-[1rem] rounded-xl shadow-3xs shadow-[#028041] hover:bg-[#028041] hover:text-[#fff] transition-colors'>
+                                Contact Me
+                            </HashLink>
+                        </div>
+
+                    </div>
+                            
+                </div>
+
+                <div className='relative w-full z-20 sm:flex-1 bg-cover h-[15rem] sm:h-100 rounded-[80px] sm:rounded-[100px]'
+                style={{backgroundImage: `url("/1.jpg")`, backgroundSize: "120%", backgroundPosition: 'top -40px center'}}>
+
+                    <div className='hidden z-2 absolute bottom-0 text-[#79af87] -left-10 text-[0.5rem] sm:grid grid-cols-5 gap-4'>
+
+                        <FaDotCircle/>
+                        <FaDotCircle/>
+                        <FaDotCircle/>
+                        <FaDotCircle/>
+                        <FaDotCircle/>
+
+                        <FaDotCircle/>
+                        <FaDotCircle/>
+                        <FaDotCircle/>
+                        <FaDotCircle/>
+                        <FaDotCircle/>
+
+                        <FaDotCircle/>
+                        <FaDotCircle/>
+                        <FaDotCircle/>
+                        <FaDotCircle/>
+                        <FaDotCircle/>
+
+                        <FaDotCircle/>
+                        <FaDotCircle/>
+                        <FaDotCircle/>
+                        <FaDotCircle/>
+                        <FaDotCircle/>
+
+                        <FaDotCircle/>
+                        <FaDotCircle/>
+                        <FaDotCircle/>
+                        <FaDotCircle/>
+                        <FaDotCircle/>
+
+                    </div>
+
+                </div>
+
+                <div className='sm:hidden flex self-start items-center gap-2 sm:gap-4 mt-4 ml-3'>
+
+                    <div className='flex gap-2'>
+
+                    <Link className='hover:scale-120 transition-all' to={"https://github.com/Robots-Pixels"}>
+                        <FaLinkedin className='text-2xl'/>
+                    </Link>
+
+                    <Link className='hover:scale-120 transition-all' to={"https://www.linkedin.com/in/otmar-tchenga-953988319?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"}>
+                        <FaGithub className='text-2xl'/>
+                    </Link>
+
+                    <Link className='hover:scale-120 transition-all' to={"mailto:otmartch23@gmail.com"}>
+                        <FaEnvelope className='text-2xl'/>
+                    </Link>
+
+                    <Link className='hover:scale-120 transition-all' to={"https://wa.me/2290140820493"}>
+                        <FaWhatsapp className='text-2xl'/>
+                    </Link>
+
+
+                    </div>
+
+                    <div className=''>
+                        <HashLink smooth to={"#contact"} className='border-[#028041] border-1 text-[#028041] px-2 py-2 text-[1rem] rounded-xl shadow-3xs shadow-[#028041] hover:bg-[#028041] hover:text-[#fff] transition-colors'>
+                            Contact Me
+                        </HashLink>
+                    </div>
+
+                </div>
+            
+            </div>
+
+        </section>
+
+        <section id="about" className="py-20 bg-[#d5fadd]">
+            <div className="max-w-3xl md:max-w-6xl mx-auto px-6 md:px-8]">
+                <h2 className="text-3xl font-semibold mb-6">More About Me</h2>
+                <p className="text-md sm:text-lg">
+                <span className='text-3xl'>I</span>'m Otmar — your partner in building powerful, scalable, and user-focused web applications. As a professional web developer with over 2 years of experience, I help businesses and entrepreneurs turn their ideas into robust digital solutions. Using top level technologies, like the MERN stack, I craft clean, efficient code and seamless user experiences — always with scalability and performance in mind. If you need a website or web app that’s fast, reliable, and designed for growth, let’s make it happen.</p>
+                <p className="mt-4 text-md sm:text-lg">
+                <span className='text-3xl'>C</span>uriosity and continuous learning fuel my craft. I stay ahead in the fast-moving tech world, constantly exploring new tools and technologies — <span className='font-bold'> so I can deliver innovative, future-proof solutions for my clients</span></p>
+                <p className="mt-4 text-md sm:text-lg">
+                <span className='text-3xl'>I</span> <span className='font-bold'>believe in collaboration, enjoy working in teams</span> and mentoring others. It's important to me to not only develop my technical skills but also embrace a startup mindset—understanding business goals and taking initiative to lead projects.</p>
+            </div>
+        </section>
+
+        <section id="portfolio" className='py-20'>
+
+            <div className="max-w-3xl md:max-w-6xl mx-auto px-6 md:px-8]">
+                <h1 className="text-3xl font-semibold mb-10">My Work</h1>
+                <div className="flex flex-col gap-5">
+
+                    <div onClick={(e) => {e.stopPropagation(); window.location.href='https://celeste-restaurant.vercel.app/'}} className="relative project overflow-hidden group">
+
+                        <div className='project-layer absolute w-full h-full bg-[#000]/60 gap-3 rounded-3xl flex flex-col items-center justify-center translate-y-[100%] group-hover:translate-y-[0%]' 
+                        style={{transition: "translate 0.7s"}}>
+
+                            <div>
+                                <h3 className='text-[#faf8ff] w-[20rem] sm:w-[30rem] text-center font-bold'>
+                                    A clean, responsive portfolio platform with dynamic sections and smooth animations.
+                                </h3>
+                            </div>
+
+                            <div className='flex items-center justify-center text-2xl gap-10'>
+
+                                <Link className='text-2xl sm:text-4xl text-black' to={"https://github.com/Robots-Pixels/Celeste-Restaurant"} target='_blank'>
+                                    <button className='flex items-center bg-[#028041] rounded-sm gap-2 p-1 text-[#faf8ff]'>
+                                        <span className='text-sm sm:text-xl'>Code Here</span>
+                                        <FaGithub className='text-[1rem] sm:text-[2rem]'/>
+                                    </button>
+                                </Link>
+
+                                <Link className='text-2xl sm:text-4xl text-black' to={"https://github.com/Robots-Pixels/Celeste-Restaurant"} target='_blank'>
+                                    <button className='flex items-center bg-[#028041] rounded-sm gap-2 p-1 text-[#faf8ff]'>
+                                        <span className='text-sm sm:text-xl'>Live Preview</span>
+                                        <FaEye className='text-[1rem] sm:text-[2rem]'/>
+                                    </button>
+                                </Link>
+
+
+                            </div>
+                        </div>
+
+                        <img className='rounded-3xl' src="/project1.png" />
+
+                    </div>
+
+                    <div onClick={(e) => {e.stopPropagation(); window.location.href='https://yumking-frontend.vercel.app/'}} className="relative project overflow-hidden group">
+
+                        <div className='project-layer absolute w-full h-full bg-[#000]/60 gap-3 rounded-3xl flex flex-col items-center justify-center translate-y-[100%] group-hover:translate-y-[0%]' 
+                        style={{transition: "translate 0.7s"}}>
+
+                            <div>
+                                <h3 className='text-[#faf8ff] w-[30rem] text-center font-bold'>
+                                    A fast and intuitive food delivery app with real-time reservation, smart filters and authentication.
+                                </h3>
+                            </div>
+
+                            <div className='flex items-center justify-center text-2xl gap-10'>
+
+                                <Link className='text-2xl sm:text-4xl text-black' to={"https://github.com/Robots-Pixels/yumking-fast-food-app"} target='_blank'>
+                                    <button className='flex items-center bg-[#028041] rounded-sm gap-2 p-1 text-[#faf8ff]'>
+                                        <span className='text-sm sm:text-xl'>Code Here</span>
+                                        <FaGithub className='text-[1rem] sm:text-[2rem]'/>
+                                    </button>
+                                </Link>
+
+                                <Link className='text-2xl sm:text-4xl text-black' to={"https://yumking-frontend.vercel.app/"} target='_blank'>
+                                    <button className='flex items-center bg-[#028041] rounded-sm gap-2 p-1 text-[#faf8ff]'>
+                                        <span className='text-sm sm:text-xl'>Live Preview</span>
+                                        <FaEye className='text-[1rem] sm:text-[2rem]'/>
+                                    </button>
+                                </Link>
+
+
+                            </div>
+                        </div>
+
+                        <img className='rounded-3xl' src="/project2.png" />
+
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+
+        <section id="skills" className='py-20 bg-[#d5fadd]'>
+
+            <div className="max-w-3xl md:max-w-6xl mx-auto px-6 md:px-8]">
+
+                <div id="services">
+
+                    <div className="container">
+                        <h1 className="text-3xl font-bold">My Services</h1>
+                        <div className="services-list">
+                            
+                        <div>
+                            <i className="fa-brands fa-app-store"></i>
+                            <h2>FullStack Web Development</h2>
+                            <p>
+                            Building complete, scalable web applications from idea to deployment using the MERN stack.
+                            </p>
+
+                            <HashLink smooth to="#contact" className='flex items-center gap-2 text-sm'> <span>Let's Get in touch </span> <span><FaArrowRight/></span> </HashLink>
+
+                        </div>
+                    
+                        <div>
+                            <i className="fa-brands fa-app-store"></i>
+                            <h2>Front-End Development</h2>
+                            <p>
+                            Crafting responsive, user-friendly interfaces with React and modern design principles.
+                            </p>
+
+                            <HashLink smooth to="#contact" className='flex items-center gap-2 text-sm' > <span>Let's Get in touch </span> <span><FaArrowRight/></span> </HashLink>
+
+                        </div>
+                             
+                        <div>
+                            <i className="fa-brands fa-app-store"></i>
+                            <h2>Back-End Development</h2>
+                            <p>
+                            Designing robust APIs, managing databases, and ensuring secure, high-performance server-side logic with Node.js and MongoDB.
+                            </p>
+
+                            <HashLink smooth to="#contact" className='flex items-center gap-2 text-sm'> <span>Let's Get in touch </span> <span><FaArrowRight/></span> </HashLink>
+
+                        </div>
+                        
+                        </div>
+                    </div>
+                    
+                </div>
+
+            </div>
+
+        </section>
+
+        <section id='contact'>
+            <div className="max-w-3xl md:max-w-6xl mx-auto px-6 md:px-8] py-20">
+                <div className="flex flex-col items-center gap-5">
+                    <h2 className='text-2xl sm:text-4xl font-bold text-[#028041] text-center mb-4'>Send me a message!</h2>
+
+                    <h3 className='text-md sm:text-xl text-center mb-4'>
+                    Have a project in mind or just want to connect? Let's talk.
+                    </h3>
+                    
+                    <form className='flex flex-col gap-6' onSubmit={handleSubmit}>
+
+                        <div className='flex flex-col sm:flex-row gap-8'>
+
+                            <div className='flex flex-col gap-1'>
+                                <label className='text-xs sm:text-md'>Your Name</label>
+                                <input id='name' onChange={handleChange} value={formData.name} required className='text-md sm:text-md outline-none pb-2 border-b-1 w-full text-[#052935]' type="text" placeholder='Enter your name'/>
+                            </div>
+
+                            <div className='flex flex-col gap-1'>
+                                <label className='text-xs sm:text-md'>Your Email Address</label>
+                                <input id='email' onChange={handleChange} value={formData.email} required className='text-md sm:text-md outline-none pb-2 border-b-1 w-full text-[#052935]' type="email" placeholder='Enter your email'/>
+                            </div>
+
+                        </div>
+
+                        <div className='flex flex-col gap-1 flex-1'>
+                            <label className='text-xs sm:text-md'>Your Message</label>
+                            <textarea id='message' onChange={handleChange} value={formData.message} rows={5} required className='text-md sm:text-md outline-none pb-2 border-b-1 text-[#052935]' type="text" placeholder="Hi, we're looking for someone who can help us build fast web app at Company X. When would you be available to discuss this?"/>
+                        </div>
+
+                        <div>
+                            
+                        </div>
+
+                        <button className={`${loading ? "opacity-90" : ""} mx-auto border-[#028041] border-1 p-2 text-[#028041] hover:bg-[#028041] hover:text-[#faf8ff] transition-all rounded-xl w-full sm:w-[70%]`}>
+                            Send
+                        </button>
+
+                    </form>
+
+                </div>
+            </div>
+        </section>
+
+        <Footer/>
+        
+    </div>
+
+  )
+}
